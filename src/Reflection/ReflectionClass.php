242a9542f0e9ada276ebebb36ae6d912c0b17447
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-07-05 08:34:42 +0000

    just written all the code and tests, not run it yet :fingers-crossed:

--- src/Reflection/ReflectionClass.php
+++ src/Reflection/ReflectionClass.php
@@ -4,5 +4,398 @@ namespace ts\Reflection;
 
 class ReflectionClass
 {
+    /**
+     * @var \ReflectionClass
+     */
+    private $reflectionClass;
 
+    /**
+     * @var \ReflectionClass
+     */
+    private static $selfReflection;
+
+    /**
+     * ReflectionClass constructor.
+     *
+     * @param string $fqn
+     *
+     * @throws \ReflectionException
+     */
+    public function __construct(string $fqn)
+    {
+        $this->reflectionClass = new \ReflectionClass($fqn);
+    }
+
+    /**
+     * @return \ReflectionClass
+     * @throws \ReflectionException
+     */
+    private function getSelfReflection(): \ReflectionClass
+    {
+        if (null === self::$selfReflection) {
+            self::$selfReflection = new \ReflectionClass(self::class);
+        }
+
+        return self::$selfReflection;
+    }
+
+    /**
+     * For when we need to return instances of self from an existing instance of \ReflectionClass
+     *
+     * @param \ReflectionClass $reflectionClass
+     *
+     * @return self
+     * @throws \ReflectionException
+     */
+    private function constructFromReflectionClass(\ReflectionClass $reflectionClass): self
+    {
+        /**
+         * @var $instance self
+         */
+        $instance                  = $this->getSelfReflection()->newInstanceWithoutConstructor();
+        $instance->reflectionClass = $reflectionClass;
+
+        return $instance;
+    }
+
+    /**
+     * Take an array of raw \ReflectionClass objects and return an array of self
+     *
+     * @param array $reflectionClasses
+     *
+     * @return array|self[]
+     * @throws \ReflectionException
+     */
+    private function convertArrayOfReflectionToSelf(array $reflectionClasses): array
+    {
+        $return = [];
+        foreach ($reflectionClasses as $reflectionClass) {
+            $return[] = $this->constructFromReflectionClass($reflectionClass);
+        }
+
+        return $return;
+    }
+
+    /**
+     * @param string $fqn
+     * @param bool   $return
+     *
+     * @return string
+     */
+    public static function export(string $fqn, $return = false): string
+    {
+        return \ReflectionClass::export($fqn, $return);
+    }
+
+    public function __toString(): string
+    {
+        return $this->reflectionClass->__toString();
+    }
+
+    public function getName(): string
+    {
+        return $this->reflectionClass->getName();
+    }
+
+    public function isInternal(): bool
+    {
+        return $this->reflectionClass->isInternal();
+    }
+
+    public function isUserDefined(): bool
+    {
+        return $this->reflectionClass->isUserDefined();
+    }
+
+    public function isInstantiable(): bool
+    {
+        return $this->reflectionClass->isInstantiable();
+    }
+
+    public function isCloneable(): bool
+    {
+        return $this->reflectionClass->isCloneable();
+    }
+
+    public function getFileName(): string
+    {
+        return $this->reflectionClass->getFileName();
+    }
+
+    public function getStartLine(): int
+    {
+        return $this->reflectionClass->getStartLine();
+    }
+
+    public function getEndLine(): int
+    {
+        return $this->reflectionClass->getEndLine();
+    }
+
+    public function getDocComment(): string
+    {
+        return (string)$this->reflectionClass->getDocComment();
+    }
+
+    public function getConstructor(): \ReflectionMethod
+    {
+        return $this->reflectionClass->getConstructor();
+    }
+
+    public function hasMethod(string $name): bool
+    {
+        return $this->reflectionClass->hasMethod($name);
+    }
+
+    public function getMethod(string $name): \ReflectionMethod
+    {
+        return $this->reflectionClass->getMethod($name);
+    }
+
+    public function getMethods(?int $filter = null): array
+    {
+        return $this->reflectionClass->getMethods($filter);
+    }
+
+    public function hasProperty(string $name): bool
+    {
+        return $this->reflectionClass->hasProperty($name);
+    }
+
+    public function getProperty(string $name): \ReflectionProperty
+    {
+        return $this->reflectionClass->getProperty($name);
+    }
+
+    /**
+     * @param int|null $filter
+     *
+     * @return array|\ReflectionProperty[]
+     */
+    public function getProperties(?int $filter = null): array
+    {
+        return $this->reflectionClass->getProperties($filter);
+    }
+
+    public function getReflectionConstant(string $name): \ReflectionClassConstant
+    {
+        return $this->reflectionClass->getReflectionConstant($name);
+    }
+
+    /**
+     * @return array|\ReflectionClassConstant[]
+     */
+    public function getReflectionConstants(): array
+    {
+        return $this->reflectionClass->getReflectionConstants();
+    }
+
+    public function hasConstant(string $name): bool
+    {
+        return $this->reflectionClass->hasConstant($name);
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function getConstants(): array
+    {
+        return $this->reflectionClass->getConstants();
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function getConstant(string $name)
+    {
+        return $this->reflectionClass->getConstant($name);
+    }
+
+    /**
+     * @return array|self[]
+     * @throws \ReflectionException
+     */
+    public function getInterfaces(): array
+    {
+        return $this->convertArrayOfReflectionToSelf($this->reflectionClass->getInterfaces());
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function getInterfaceNames(): array
+    {
+        return $this->reflectionClass->getInterfaceNames();
+    }
+
+    public function isAnonymous(): bool
+    {
+        return $this->reflectionClass->isAnonymous();
+    }
+
+    public function isInterface(): bool
+    {
+        return $this->reflectionClass->isInterface();
+    }
+
+    /**
+     * @return array|self[]
+     * @throws \ReflectionException
+     */
+    public function getTraits(): array
+    {
+        return $this->convertArrayOfReflectionToSelf($this->reflectionClass->getTraits());
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function getTraitNames(): array
+    {
+        return $this->reflectionClass->getTraitNames();
+    }
+
+    public function getTraitAliases(): array
+    {
+        return $this->reflectionClass->getTraitAliases();
+    }
+
+    public function isTrait(): bool
+    {
+        return $this->reflectionClass->isTrait();
+    }
+
+    public function isAbstract(): bool
+    {
+        return $this->reflectionClass->isAbstract();
+    }
+
+    public function isFinal(): bool
+    {
+        return $this->reflectionClass->isFinal();
+    }
+
+    public function getModifiers(): int
+    {
+        return $this->reflectionClass->getModifiers();
+    }
+
+    public function isInstance($object): bool
+    {
+        return $this->reflectionClass->isInstance($object);
+    }
+
+    /**
+     * @param mixed ...$args
+     *
+     * @return object
+     */
+    public function newInstance(...$args)
+    {
+        return $this->reflectionClass->newInstance(...$args);
+    }
+
+    /**
+     * @return object
+     */
+    public function newInstanceWithoutConstructor()
+    {
+        return $this->reflectionClass->newInstanceWithoutConstructor();
+    }
+
+    /**
+     * @param array|null $args
+     *
+     * @return object
+     */
+    public function newInstanceArgs(array $args = null)
+    {
+        return $this->reflectionClass->newInstanceArgs($args);
+    }
+
+    /**
+     * @return self
+     * @throws \ReflectionException
+     */
+    public function getParentClass(): self
+    {
+        return $this->constructFromReflectionClass($this->reflectionClass->getParentClass());
+    }
+
+    public function isSubclassOf(string $class): bool
+    {
+        return $this->reflectionClass->isSubclassOf($class);
+    }
+
+    public function getStaticProperties(): array
+    {
+        return $this->reflectionClass->getStaticProperties();
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    public function getStaticPropertyValue(string $name, $default = null)
+    {
+        return $this->reflectionClass->getStaticPropertyValue($name, $default);
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $value
+     */
+    public function setStaticPropertyValue(string $name, $value): void
+    {
+        $this->reflectionClass->setStaticPropertyValue($name, $value);
+    }
+
+    public function getDefaultProperties(): array
+    {
+        return $this->reflectionClass->getDefaultProperties();
+    }
+
+    public function isIterateable(): bool
+    {
+        return $this->reflectionClass->isIterateable();
+    }
+
+    public function isIterable(): bool
+    {
+        return $this->reflectionClass->isIterable();
+    }
+
+    public function implementsInterface(string $interface): bool
+    {
+        return $this->reflectionClass->implementsInterface($interface);
+    }
+
+    public function getExtension(): \ReflectionExtension
+    {
+        return $this->reflectionClass->getExtension();
+    }
+
+    public function getExtensionName(): string
+    {
+        return (string)$this->reflectionClass->getExtensionName();
+    }
+
+    public function inNamespace(): bool
+    {
+        return $this->reflectionClass->inNamespace();
+    }
+
+    public function getNamespaceName(): string
+    {
+        return $this->reflectionClass->getNamespaceName();
+    }
+
+    public function getShortName(): string
+    {
+        return $this->reflectionClass->getShortName();
+    }
 }
--- tests/ts/Reflection/ReflectionClassTest.php
+++ tests/ts/Reflection/ReflectionClassTest.php
@@ -1,10 +1,406 @@
 <?php declare(strict_types=1);
 
-namespace ts\Reflection;
+namespace ts\Tests\small\Reflection;
 
 use PHPUnit\Framework\TestCase;
+use ts\Reflection\ReflectionClass;
 
 class ReflectionClassTest extends TestCase
 {
 
+    private const FAKE = 'value';
+
+    private static $fake = 'value';
+
+    /**
+     * @var ReflectionClass
+     */
+    private static $instance;
+
+    /**
+     * @var \ReflectionClass
+     */
+    private static $raw;
+
+    /**
+     * @throws \ReflectionException
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$instance = new ReflectionClass(self::class);
+        self::$raw      = new \ReflectionClass(self::class);
+    }
+
+    public function testGetExtensionName()
+    {
+        $expected = '';
+        $actual   = self::$instance->getExtensionName();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetName()
+    {
+        $expected = self::class;
+        $actual   = self::$instance->getName();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsInterface()
+    {
+        $expected = false;
+        $actual   = self::$instance->isInterface();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetExtension()
+    {
+        $expected = self::$raw->getExtension();
+        $actual   = self::$instance->getExtension();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetReflectionConstant()
+    {
+        $expected = self::FAKE;
+        $actual   = self::$instance->getReflectionConstant('FAKE');
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetStaticPropertyValue()
+    {
+        $expected = self::$fake;
+        $actual   = self::$instance->getStaticPropertyValue('fake');
+        self::assertSame($expected, $actual);
+    }
+
+    public function testNewInstanceArgs()
+    {
+        $expected = new \SplBool();
+        $actual   = (new ReflectionClass('\SplBool'))->newInstanceArgs();
+        self::assertEquals($expected, $actual);
+    }
+
+    public function testIsUserDefined()
+    {
+        $expected = true;
+        $actual   = self::$instance->isUserDefined();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetConstants()
+    {
+        $expected = ['FAKE' => self::FAKE];
+        $actual   = self::$instance->getConstants();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testHasConstant()
+    {
+        $has      = 'FAKE';
+        $expected = true;
+        $actual   = self::$instance->hasConstant($has);
+        self::assertSame($expected, $actual);
+        $expected = false;
+        $notHas   = 'NOT';
+        $actual   = self::$instance->hasConstant($notHas);
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsFinal()
+    {
+        $expected = false;
+        $actual   = self::$instance->isFinal();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testImplementsInterface()
+    {
+        $expected = false;
+        $actual   = self::$instance->implementsInterface(\Iterator::class);
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsSubclassOf()
+    {
+        $expected = false;
+        $actual   = self::$instance->isSubclassOf(\IteratorIterator::class);
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetFileName()
+    {
+        $expected = __FILE__;
+        $actual   = self::$instance->getFileName();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetMethods()
+    {
+        $expected = self::$raw->getMethods();
+        $actual   = self::$instance->getMethods();
+        self::assertEquals($expected, $actual);
+    }
+
+    public function testGetReflectionConstants()
+    {
+        $expected = self::$raw->getReflectionConstants();
+        $actual   = self::$instance->getReflectionConstants();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetStartLine()
+    {
+        $expected = self::$raw->getStartLine();
+        $actual   = self::$instance->getStartLine();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetStaticProperties()
+    {
+        $expected = self::$raw->getStaticProperties();
+        $actual   = self::$instance->getStaticProperties();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testSetStaticPropertyValue()
+    {
+        $expected = 'updated'
+        $actual   = self::$instance->setStaticPropertyValue('fake', $expected);
+        self::assertSame($expected, self::$instance->getStaticPropertyValue('fake'));
+        self::$instance->setStaticPropertyValue('fake', 'value');
+    }
+
+    public function testGetDocComment()
+    {
+        $expected = '';
+        $actual   = self::$instance->getDocComment();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testNewInstance()
+    {
+        $expected = new \SplBool();
+        $actual   = (new ReflectionClass(\SplBool::class))->newInstance();
+        self::assertEquals($expected, $actual);
+    }
+
+    public function testIsInternal()
+    {
+        $expected = false;
+        $actual   = self::$instance->isInternal();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetTraitNames()
+    {
+        $expected = self::$raw->getTraitNames();
+        $actual   = self::$instance->getTraitNames();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsTrait()
+    {
+        $expected = false;
+        $actual   = self::$instance->isTrait();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsInstance()
+    {
+        $object   = new \SplBool();
+        $expected = false;
+        $actual   = self::$instance->isInstance($object);
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsAbstract()
+    {
+        $expected = false;
+        $actual   = self::$instance->isAbstract();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsIterable()
+    {
+        $expected = false;
+        $actual   = self::$instance->isIterable();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetConstructor()
+    {
+        $expected = self::$raw->getConstructor();
+        $actual   = self::$instance->getConstructor();
+        self::assertEquals($expected, $actual);
+    }
+
+    public function testHasProperty()
+    {
+        $expected = false;
+        $actual   = self::$instance->hasProperty('no');
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetInterfaces()
+    {
+        $expected = self::$raw->getInterfaces();
+        $actual   = self::$instance->getInterfaces();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsCloneable()
+    {
+        $expected = self::$raw->isCloneable();
+        $actual   = self::$instance->isCloneable();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetProperties()
+    {
+        $filter   = \ReflectionProperty::IS_PUBLIC | \ReflectionProperty::IS_PRIVATE;
+        $expected = self::$raw->getProperties($filter);
+        $actual   = self::$instance->getProperties($filter);
+        self::assertSame($expected, $actual);
+    }
+
+    public function testNewInstanceWithoutConstructor()
+    {
+        $expected = new \SplBool();
+        $actual   = (new ReflectionClass(\SplBool::class))->newInstanceWithoutConstructor();
+        self::assertEquals($expected, $actual);
+    }
+
+    public function testGetParentClass()
+    {
+        $expected = self::$raw->getParentClass();
+        $actual   = self::$instance->getParentClass();
+        self::assertSame($expected, $actual);
+
+    }
+
+    public function testGetInterfaceNames()
+    {
+        $expected = self::$raw->getInterfaceNames();
+        $actual   = self::$instance->getInterfaceNames();
+        self::assertSame($expected, $actual);
+    }
+
+    public function test__toString()
+    {
+        $expected = self::$raw->__toString();
+        $actual   = self::$instance->__toString();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetModifiers()
+    {
+        $expected = self::$raw->getModifiers();
+        $actual   = self::$instance->getModifiers();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testExport()
+    {
+        $expected = self::$raw::export(self::class);
+        $actual   = self::$instance::export(self::class);
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetDefaultProperties()
+    {
+        $expected = self::$raw->getDefaultProperties();
+        $actual   = self::$instance->getDefaultProperties();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetProperty()
+    {
+        $expected = self::$raw->getProperty('fake');
+        $actual   = self::$instance->getProperty('fake');
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsIterateable()
+    {
+        $expected = self::$raw->isIterable();
+        $actual   = self::$instance->isIterable();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsInstantiable()
+    {
+        $expected = self::$raw->isInstantiable();
+        $actual   = self::$instance->isInstantiable();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetTraitAliases()
+    {
+        $expected = self::$raw->getTraitAliases();
+        $actual   = self::$instance->getTraitAliases();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetTraits()
+    {
+        $expected = self::$raw->getTraits();
+        $actual   = self::$instance->getTraits();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetNamespaceName()
+    {
+        $expected = self::$raw->getNamespaceName();
+        $actual   = self::$instance->getNamespaceName();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetMethod()
+    {
+        $expected = self::$raw->getMethod(__FUNCTION__);
+        $actual   = self::$instance->getMethod(__FUNCTION__);
+        self::assertSame($expected, $actual);
+    }
+
+    public function testInNamespace()
+    {
+        $expected = self::$raw->inNamespace();
+        $actual   = self::$instance->inNamespace();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testHasMethod()
+    {
+        $expected = self::$raw->hasMethod(__FUNCTION__);
+        $actual   = self::$instance->hasMethod(__FUNCTION__);
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetShortName()
+    {
+        $expected = self::$raw->getShortName();
+        $actual   = self::$instance->getShortName();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetConstant()
+    {
+        $expected = self::$raw->getConstant('FAKE');
+        $actual   = self::$instance->getConstant('FAKE');
+        self::assertSame($expected, $actual);
+    }
+
+    public function testGetEndLine()
+    {
+        $expected = self::$raw->getEndLine();
+        $actual   = self::$instance->getEndLine();
+        self::assertSame($expected, $actual);
+    }
+
+    public function testIsAnonymous()
+    {
+        $expected = self::$raw->isAnonymous();
+        $actual   = self::$instance->isAnonymous();
+        self::assertSame($expected, $actual);
+    }
 }
