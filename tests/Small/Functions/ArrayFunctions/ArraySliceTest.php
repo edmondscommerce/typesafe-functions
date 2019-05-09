6d5c2da77843dd48e8816dc44acf2ff8b1488bc8
catalin <catalin@catalin>
2019-05-09 15:10:50 +0000

    array slice tests

--- tests/Small/Functions/ArrayFunctions/ArraySliceTest.php
+++ tests/Small/Functions/ArrayFunctions/ArraySliceTest.php
@@ -9,5 +9,68 @@ use PHPUnit\Framework\TestCase;
  */
 class ArraySliceTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function itSlicesArrayOffset(): void
+    {
+        $array = ['a', 'b', 'c', 'd', 'e', 'f'];
 
+        $expected = ['b', 'c', 'd', 'e', 'f'];
+        $actual   = \ts\array_slice($array, 1);
+
+        self::assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itSlicesArrayOffsetLength(): void
+    {
+        $array = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+        $expected = ['b', 'c'];
+        $actual   = \ts\array_slice($array, 1, 2);
+
+        self::assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itSlicesArrayOffsetLengthPreserve(): void
+    {
+        $array = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+        $expected = ['1' => 'b', '2' => 'c'];//, 'd', 'e', 'f'];
+        $actual   = \ts\array_slice($array, 1, 2, true);
+
+        self::assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itSlicesArrayOffsetLengthPreserveOff(): void
+    {
+        $array = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+        $expected = ['0' => 'b', '1' => 'c'];//, 'd', 'e', 'f'];
+        $actual   = \ts\array_slice($array, 1, 2, false);
+
+        self::assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itDoesNotCombineDifferentLengthArrays(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage("Slice is empty in");
+
+        $array = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+        @\ts\array_slice($array, 1, 0);
+    }
 }
