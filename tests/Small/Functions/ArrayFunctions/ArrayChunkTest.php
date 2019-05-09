921d0d805c562d3af28327aa37d09c9ecedaa9f8
dale <dale@dale>
2019-05-09 17:14:41 +0000

    Made a start on array_chunk

---
+++ tests/Small/Functions/ArrayFunctions/ArrayChunkTest.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace ts\Tests\Small\Functions\ArrayFunctions;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @small
+ */
+class ArrayChunkTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itCanChunkAnArrayInToMultipleValues(): void
+    {
+        $input = range(1, 10);
+
+        $expected = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]];
+
+        $actual = \ts\array_chunk($input, 2);
+
+        foreach ($actual as $item) {
+            self::assertCount(2, $item);
+        }
+
+        self::assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itCanChunkAnArrayInToMultipleValuesWithAnNonMatchingFinalChunk(): void
+    {
+        $input = range(1, 9);
+
+        $expected = [[1, 2], [3, 4], [5, 6], [7, 8], [9]];
+
+        $actual = \ts\array_chunk($input, 2);
+
+        self::assertSame($expected, $actual);
+
+        $finalChunk = array_pop($actual);
+
+        self::assertCount(1, $finalChunk);
+
+        foreach ($actual as $item) {
+            self::assertCount(2, $item);
+        }
+    }
+}
