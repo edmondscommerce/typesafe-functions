bdfc1cd3dc7487f8975cbc2da6f53320eae57df1
dale <dale@dale>
2019-05-15 16:17:09 +0000

    Adds tests for with and without preserve keys on array chunk

--- tests/Small/Functions/ArrayFunctions/ArrayChunkTest.php
+++ tests/Small/Functions/ArrayFunctions/ArrayChunkTest.php
@@ -12,9 +12,12 @@ class ArrayChunkTest extends TestCase
     /**
      * @test
      */
-    public function itCanChunkAnArrayInToMultipleValues(): void
+    public function itCanChunkAnArrayInToMultipleValuesWithoutPreservingTheKeys(): void
     {
-        $input = range(1, 10);
+        $numbers = range(1, 10);
+        $letters = str_split('abcdefghij');
+
+        $input = \ts\array_combine($letters, $numbers);
 
         $expected = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]];
 
@@ -47,5 +50,26 @@ class ArrayChunkTest extends TestCase
         foreach ($actual as $item) {
             self::assertCount(2, $item);
         }
+    }
+
+    /**
+     * @test
+     */
+    public function itCanChunkAnArrayInToMultipleValuesAndPreserveTheKeys(): void
+    {
+        $numbers = range(1, 10);
+        $letters = str_split('abcdefghij');
+
+        $input = \ts\array_combine($letters, $numbers);
+
+        $expected = [['a' => 1, 'b' => 2], ['c' => 3, 'd' => 4], ['e' => 5, 'f' => 6], ['g' => 7, 'h' => 8], ['i' => 9, 'j' => 10]];
+
+        $actual = \ts\array_chunk($input, 2, true);
+
+        foreach ($actual as $item) {
+            self::assertCount(2, $item);
+        }
+
+        self::assertSame($expected, $actual);
     }
 }
